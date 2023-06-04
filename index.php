560a3a2364b970ee5fb0ec2bab0488fbb2f3faea
cogwizzle <joseph.fehrman@gmail.com>
2023-06-03 22:23:54 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<form action="roll.php" method="post">
+  <input type="dice-description" name="dice-description" placeholder="Dice Description">
+  <input type="submit" name="roll" value="Roll">
+</form>
