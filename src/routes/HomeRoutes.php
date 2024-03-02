d5d7e9f7bf82c9b7dac6e52db459efe4584bce0f
Joe Fehrman <joseph.fehrman@gmail.com>
2024-03-01 21:32:16 -0500

    feat: main layout done.

---
+++ src/routes/HomeRoutes.php
@@ -0,0 +1,17 @@
+<?php namespace App\Routes;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App as App;
+use App\Routes\Controllers\HomeController;
+
+require_once __DIR__ . '/Controllers/HomeController.php';
+
+function HomeRoutes(App $app) {
+    $app->get('/', function(Request $request, Response $response) use ($app) {
+        return HomeController::index($request, $response, $app); 
+    });
+
+    $app->get('/roll', function(Request $request, Response $response) use ($app) {
+        return HomeController::roll($request, $response, $app);
+    });
+}
